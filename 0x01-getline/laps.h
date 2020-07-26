df7e0e6a17146a5aaab15e7264d872af87bc6bf8
Fernando Gonzalez-Morales <fernando.gonzalez@holbertonschool.com>
2020-07-25 20:31:36 -0700

    Betty fix for header file

--- 0x01-getline/laps.h
+++ 0x01-getline/laps.h
@@ -4,13 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#ifdef __INT_MAX__
-#define INTMAX __INT_MAX__
-#else
-#define INTMAX ((2**32) / 2)
-#endif
-
 void race_state(int *id, size_t size);
-
 
 #endif /* _LAPS_H_ */
