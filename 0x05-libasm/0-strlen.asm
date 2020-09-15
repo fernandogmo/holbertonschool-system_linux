e02682bfdee3c623b7a2f73c3f0038bd05dab767
Fernando Gonzalez-Morales <fernando.gonzalez@holbertonschool.com>
2020-09-15 12:06:25 -0700

    Minimal version of Task 0

--- 0x05-libasm/0-strlen.asm
+++ 0x05-libasm/0-strlen.asm
@@ -1,18 +1,13 @@
-; This program calculates the length of a string as a size_t
-section .text
 global asm_strlen
 asm_strlen:
-	enter	64, 0
+	enter	0, 0
 	xor	rax, rax
 	cmp	BYTE [rdi], 0
 	je	.DONE
-
 .LOOP:
-	add 	rax, 1
+	inc	rax
 	cmp	BYTE [rdi+rax], 0
 	jne	.LOOP
-	jmp	.DONE
-
 .DONE:
 	leave
 	ret
