8f0c64f542caf9f4ee0f5eccf59085742f224ead
Fernando Gonzalez-Morales <fernando.gonzalez@holbertonschool.com>
2020-09-19 15:41:23 -0700

    Cleaned up Task 8

--- 0x05-libasm/8-strcasecmp.asm
+++ 0x05-libasm/8-strcasecmp.asm
@@ -1,42 +1,28 @@
 GLOBAL asm_strcasecmp
 asm_strcasecmp:
-	push rbp
-	mov rbp, rsp
-	push rcx
-	push r8
-	push r9
-
-	xor rcx, rcx
-	xor r8, r8
-	xor r9, r9
-	xor eax, eax
-loop_asm_strcasecmp:
-	mov r8b, [rdi + rcx]
-	mov r9b, [rsi + rcx]
-	cmp r8b, 65		; begin checking r8b
-	jl check_9		; rdi + rcx < 'A'
-	cmp r8b, 90
-	jg check_9		; rdi + rcx > 'Z'
-	add r8b, 32
-check_9:
-	cmp r9b, 65
-	jl cont			; rsi + rcx < 'A'
-	cmp r9b, 90
-	jg cont			; rsi + rcx > 'Z'
-	add r9b, 32
-cont:
-	mov eax, r8d
-	sub eax, r9d
-	test eax, eax		; not 0 if difference
-	jnz end
-	test r8b, r8b
-	jz end
-	inc rcx
-	jmp loop_asm_strcasecmp
-end:
-	pop r9
-	pop r8
-	pop rcx
-	mov rsp, rbp
-	pop rbp
+	xor	rcx, rcx
+.MAINLOOP:
+	movzx	r8d, BYTE [rdi + rcx]
+	movzx	r9d, BYTE [rsi + rcx]
+	cmp	r8b, 'A'
+	jl	.2ndSTRING
+	cmp	r8b, 'Z'
+	jg	.2ndSTRING
+	add	r8b, 'a' - 'A'
+.2ndSTRING:
+	cmp	r9b, 'A'
+	jl	.DIFF
+	cmp	r9b, 'Z'
+	jg	.DIFF
+	add	r9b, 'a' - 'A'
+.DIFF:
+	mov	eax, r8d
+	sub	eax, r9d
+	test	eax, eax
+	jnz	.RETURN
+	test	r8b, r8b
+	jz	.RETURN
+	inc	rcx
+	jmp	.MAINLOOP
+.RETURN:
 	ret
