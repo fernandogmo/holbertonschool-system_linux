2c53abc3ae4dbb557bb64a92b0098c5c08e45501
Fernando Gonzalez-Morales <fernando.gonzalez@holbertonschool.com>
2020-09-22 17:36:06 -0700

    Updated and documented Task 8

--- 0x05-libasm/8-strcasecmp.asm
+++ 0x05-libasm/8-strcasecmp.asm
@@ -1,23 +1,24 @@
 GLOBAL asm_strcasecmp
 asm_strcasecmp:
-        xor     edx, edx
-.LOOP:
-        mov     cl, BYTE [rdi+rdx]
-        mov     al, BYTE [rsi+rdx]
-        lea     r9d, [rcx-'A']
-        lea     r8d, [rcx+'a'-'A']
-        cmp     r9b, 'z'-'a'
-        cmovb   ecx, r8d
-        lea     r9d, [rax-'A']
-        lea     r8d, [rax+'a'-'A']
-        cmp     r9b, 'z'-'a'
-        cmovnb  r8d, eax
-        movsx   eax, cl
-        movsx   r8d, r8b
-        sub     eax, r8d
-        jne     .RETURN
-        inc     rdx
-        test    cl, cl
-        jne     .LOOP
+        xor     r8d, r8d	; zero counter
+        jmp     .NEXT		; start loop at counter=0
+.COMPARE:
+        jecxz	.RETURN		; return if char1 == '\0'
+        inc	r8
+.NEXT:
+        movzx	ecx, BYTE [rdi+r8]	; read next char string 1
+        movzx	edx, BYTE [rsi+r8]	; read next char string 2
+        lea	r9d, [rcx-'A']		; lower bound of char1 from uppercase range
+        lea	eax, [rcx+'a'-'A']	; get possible lowercase for char1
+        cmp	r9b, 'Z'-'A'		; if char1 was uppercase...
+        cmovbe	ecx, eax		; ...set char1 to lowercase
+        lea	r9d, [rdx-'A']		; same for char2
+        lea	eax, [rdx+'a'-'A']	; ...
+        cmp	r9b, 'Z'-'A'		; ...
+        cmovbe	edx, eax		; ...
+        movzx	eax, cl		; store char1
+        movzx	edx, dl		; store char2
+        sub	eax, edx	; store diff as return value
+        je	.COMPARE
 .RETURN:
         ret
