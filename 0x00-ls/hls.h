4623d52ac14fa9f3d9e91c7ad3009df65ce3077b
Fernando Gonzalez-Morales <fernando.gonzalez@holbertonschool.com>
2020-12-03 04:18:53 -0800

    Adding preliminary version that triggered betty bug

---
+++ 0x00-ls/hls.h
@@ -0,0 +1,13 @@
+#ifndef HLS_H
+#define HLS_H
+
+#include <stdio.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
+
+int create_fullpath(char *buf, const char *path, char *file);
+
+#endif /* HLS_H */
--- 0x00-ls/main.c
+++ 0x00-ls/main.c
@@ -1,29 +1,62 @@
-#include <stdio.h>
-#include <dirent.h>
-#include <stdlib.h>
-#include <sys/stat.h>
+#include "hls.h"
 
 int main(int argc, char const *argv[])
 {
-    if (argc < 2)
-    {
-        printf("Usage: hls [OPTION]... [FILE]...\n");
-        exit(1);
-    }
-    else
-    {
-        DIR *dirp = opendir(argv[1]);
-        struct dirent *entp;
+	if (argc < 2)
+	{
+		printf("Usage: hls [OPTION]... [FILE]...\n");
+		exit(1);
+	}
+	else
+	{
+		DIR *dirp = opendir(argv[1]);
+		struct dirent *entp;
 
-        while ((entp = readdir(dirp)) != NULL)
-        {
-            struct stat *buf = NULL;
+		while ((entp = readdir(dirp)) != NULL)
+		{
+			struct stat statbuf;
+			char *pathbuf = NULL;
 
-            lstat(argv[1], buf);
-            printf("%s\n", entp->d_name);
-        }
-        exit(0);
-    }
-    /* Should be unreachable */
-    return 127;
+			if (create_fullpath(pathbuf, argv[1], entp->d_name) != 0)
+			{
+				fprintf(stderr, "create_fullpath failed");
+				exit(1);
+			}
+
+			if (lstat(pathbuf, &statbuf) != 0)
+			{
+				fprintf(stderr, "lstat failed");
+				exit(1);
+			}
+			else
+			{
+				off_t size = statbuf.st_size;
+
+				printf("%s %lu\n", entp->d_name, size);
+			}
+		}
+		exit(0);
+	}
+	/* Should be unreachable */
+	return (127);
+}
+
+int create_fullpath(char *buf, const char *path, char *file)
+{
+	size_t len_path = strlen(path);
+	size_t len_file = strlen(file);
+	size_t extra_chars = 2;
+	char *fmt = "%s%s";
+
+	if (path[len_path - 1] != '/')
+	{
+		extra_chars = 3;
+		fmt = "%s/%s";
+	}
+	buf = malloc((len_path + len_file + extra_chars) * sizeof(char));
+	if (buf == NULL)
+		return (1);
+	sprintf(buf, fmt, path, file);
+
+	return (0);
 }
