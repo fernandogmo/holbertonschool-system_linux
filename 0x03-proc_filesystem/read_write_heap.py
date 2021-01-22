e5e794c3c092c1fb580bd6a43b5f3ed62e2fe21d
Fernando Gonzalez-Morales <fernando.gonzalez@holbertonschool.com>
2021-01-21 22:44:55 -0800

    PEP 8 changes

--- 0x03-proc_filesystem/read_write_heap.py
+++ 0x03-proc_filesystem/read_write_heap.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """
-Write a script that finds a string in the heap of a running process, and replaces it.
+Finds a string in the heap of a running process and replaces it.
 
 Usage: read_write_heap.py pid search_string replace_string
     where pid is the pid of the running process and strings are ASCII.
     The script should look only in the heap of the process.
     Output: you can print whatever you think is interesting
-    On usage error, print an error message on stdout and exit with status code 1
+    On usage error, print error message on stdout and exit with status code 1
 """
 
 import sys
 
 
 def read_write_heap(pid, oldstr, newstr):
-    """ finds and replace string in heap of process """
+    """ finds and replaces string in heap of process """
     assert len(oldstr) >= len(newstr),\
         "<new-string> cannot be longer than <old-string>"
     with open("/proc/{}/maps".format(pid), 'r') as maps:
