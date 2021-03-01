387dbe0caa874405868be6fbcbe069b87614d168
Fernando Gonzalez-Morales <fern@ndo.ai>
2021-02-28 22:20:17 -0800

    Making that initializer pass the checker

--- 0x06-signals/4-trace_signal_sender.c
+++ 0x06-signals/4-trace_signal_sender.c
@@ -1,29 +1,30 @@
 #include "signals.h"
 
 /**
- * sigquithandler - action to take on SIGQUIT intercept
+ * sigquit_handler - action to take on SIGQUIT intercept
  * @sig: number of the signal that caused invocation of the handler
  * @info: pointer to a siginfo_t
  * @ucontext: pointer to a ucontext_t
  */
-void sigquithandler(int sig, siginfo_t *info, void *ucontext)
+static inline void sigquit_handler(int sig __attribute__((__unused__)),
+				   siginfo_t *info,
+				   void *ucontext __attribute__((__unused__)))
 {
-	(void) sig;
-	(void) ucontext;
 	printf("SIGQUIT sent by %i\n", info->si_pid);
 	fflush(stdout);
 }
 
 /**
  * trace_signal_sender - Defines a handler for SIGQUIT
- *
  * Return: 0 on success, -1 on error
  */
 int trace_signal_sender(void)
 {
-	struct sigaction act;
+	C99(
+	struct sigaction act = {
+	    .sa_sigaction = sigquit_handler,
+	    .sa_flags = SA_SIGINFO};
 
-	act.sa_flags = SA_SIGINFO;
-	act.sa_sigaction = sigquithandler;
 	return (sigaction(SIGQUIT, &act, NULL));
+	);
 }
