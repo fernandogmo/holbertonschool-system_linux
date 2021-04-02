048e673bf4278bee6152c0db4b82fa2663990a52
Fernando Gonzalez-Morales <fern@ndo.ai>
2021-04-02 07:10:31 -0700

    Fix regression reordering functions

--- 0x08_CPython/3-python.c
+++ 0x08_CPython/3-python.c
@@ -1,5 +1,9 @@
 #include <Python.h>
 
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
 /**
  * print_python_list - print basic info about Python list objects
  * @p: pointer to PyObject
