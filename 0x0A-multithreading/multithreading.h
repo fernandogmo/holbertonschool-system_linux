6746c8d1e4be87b4940606849a8acffe5f2a4400
Fernando Gonzalez-Morales <fern@ndo.ai>
2021-05-23 23:59:50 -0700

    Added prime prototype

--- 0x0A-multithreading/multithreading.h
+++ 0x0A-multithreading/multithreading.h
@@ -138,6 +138,7 @@ void *thread_entry(void *arg);
 int tprintf(char const *format, ...);
 void blur_portion(blur_portion_t const *portion);
 void blur_image(img_t *img_blur, img_t const *img, kernel_t const *kernel);
+list_t *prime_factors(char const *s);
 task_t *create_task(task_entry_t entry, void *param);
 void destroy_task(task_t *task);
 void *exec_tasks(list_t const *tasks);
