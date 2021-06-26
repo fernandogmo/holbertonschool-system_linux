f2897cfaef600317afa67ef4c981daf17be3cf54
Fernando Gonzalez-Morales <fern@ndo.ai>
2021-06-26 10:11:01 -0700

    Cleaned up error checking for task 1

--- 0x0C-sockets/1-server.c
+++ 0x0C-sockets/1-server.c
@@ -8,7 +8,7 @@
 #include <arpa/inet.h>
 
 #pragma GCC diagnostic ignored "-Wpedantic"
-#define TRY(op, msg) do {op; perror(msg); return (EXIT_FAILURE)} while (0)
+#define TRY(op, msg) do {if ((op) == -1) {perror(msg); return (EXIT_FAILURE);}} while (0)
 
 /**
  * main - Entry point
@@ -16,45 +16,29 @@
  */
 int main(void)
 {
-	int listen_sockid = socket(AF_INET, SOCK_STREAM, getprotobyname("tcp")->p_proto);
-	if (-1 == listen_sockid)
-	{
-		perror("failed to create socket");
-		return (EXIT_FAILURE);
-	}
+	int listen_sockid;
+	TRY(listen_sockid = socket(AF_INET, SOCK_STREAM, getprotobyname("tcp")->p_proto),
+ 	    "failed to create socket");
 
 	struct sockaddr_in addrport = (struct sockaddr_in){AF_INET, htons(12345), {htonl(INADDR_ANY)}, {0}};
-	if (-1 == bind(listen_sockid, (const struct sockaddr *)&addrport, sizeof(addrport)))
-	{
-		perror("failed to bind socket");
-		return (EXIT_FAILURE);
-	}
-	if (-1 == listen(listen_sockid, 1))
-	{
-		perror("failed to listen on port 12345");
-		return (EXIT_FAILURE);
-	}
+	TRY(bind(listen_sockid, (const struct sockaddr *)&addrport, sizeof(addrport)),
+	    "failed to bind socket");
+	TRY(listen(listen_sockid, 1), "failed to listen on port 12345");
 
 	puts("Server listening on port 12345");
 
 	struct sockaddr_in client_addr;
 	socklen_t client_addr_len = sizeof(client_addr);
-	int data_sockid = accept(listen_sockid, (struct sockaddr *)&client_addr, &client_addr_len);
-	if (-1 == data_sockid)
-	{
-		perror("failed to accept client connection");
-		return (EXIT_FAILURE);
-	}
+	int data_sockid;
+	TRY(data_sockid = accept(listen_sockid, (struct sockaddr *)&client_addr, &client_addr_len),
+	    "failed to accept client connection");
 
 	char buf[INET_ADDRSTRLEN];
 	inet_ntop(AF_INET, &client_addr.sin_addr, buf, INET_ADDRSTRLEN);
 	printf("Client connected: %s\n", buf);
 
-	if (-1 == close(listen_sockid) || -1 == close(data_sockid))
-	{
-		perror("failed to close socket");
-		return (EXIT_FAILURE);
-	}
+	TRY(close(listen_sockid), "failed to close socket");
+	TRY(close(data_sockid), "failed to close socket");
 
 	return (EXIT_SUCCESS);
 }
